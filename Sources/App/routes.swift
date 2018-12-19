26d962281daca4cb375eaabc522507eaf8bfc3f3
Norman Sutorius <us.idev.student@gmail.com>
2018-12-19 15:48:25 +0100

    search by timestamp

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -11,21 +11,28 @@ public func routes(_ router: Router) throws {
     // Basic "Hello, world!" example
     router.get("hello") { req -> String in
         let client = try! req.make(MongoClient.self)
-        mongodb_example(client: client)
+        insertNewItem(client: client)
         print("hello")
         return "Hello, world!"
     }
 
 
-    func mongodb_example(client: MongoClient ){
+    func insertNewItem(client: MongoClient ){
         let fs: FoodStuff = FoodStuff(amount: 20, description: "banane")
         print(Date().timeIntervalSince1970)
         let dailySum = DailySummary(listOfFoodStuff: [fs], timestamp: Date().timeIntervalSince1970)
-//        dailySum.listOfFoodStuff[0] = fs
-//        let collection = try! client.db("myDB").collection("myCollection", withType: FoodStuff.self)
-
         let collection = try! client.db("myDB").collection("myCollection", withType: DailySummary.self)
         let result = try! collection.insertOne(dailySum)
         print(result?.insertedId ?? "") // prints `100`
+        
+        let query: Document = ["timestamp": 1545229259.108623]
+        let documents = try! collection.find(query)
+        for d in documents {
+            print(d)
+        }
+    }
+    
+    func update(desc: String, amount: Int) -> String {
+        return "not implemented yet"
     }
 }
